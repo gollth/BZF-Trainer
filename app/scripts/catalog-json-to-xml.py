6ce11a65be15a7634d71efbfe2529f53217faa3c
Gollth <thoregoll@googlemail.com>
2019-05-12 19:52:26 +0200

    include shuffling in catalog script

--- app/scripts/catalog-json-to-xml.py
+++ app/scripts/catalog-json-to-xml.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env/python3
 
 import json
+import random
 import os
 import os.path as p
 from xml.dom.minidom import parseString
@@ -8,17 +9,20 @@ import xml.etree.ElementTree as ET
 from argparse import ArgumentParser
 
 
-def convert(file, prefix):
+def convert(file, prefix, shuffle=True):
     with open(file) as f:
         catalog = json.load(f)
 
         xmlq = ET.Element('resources')
         xmla = ET.Element('resources')
+        xmls = ET.Element('resources')
         questions = ET.SubElement(xmlq, 'string-array')
         questions.set('name', '{}_questions'.format(prefix))
         answers = ET.SubElement(xmla, 'string-array')
         answers.set('name', '{}_answers'.format(prefix))
         answers.set('formatted', 'false')
+        solutions = ET.SubElement(xmls, 'integer-array')
+        solutions.set('name', '{}_solutions'.format(prefix))
 
         for item in catalog:
             # Error Checking
@@ -29,9 +33,19 @@ def convert(file, prefix):
             if len(item['answers']) != 4:
                 raise ValueError('Question {} has not 4 but {} answers.'.format(item['question']['number'], len(item['answers'])))
 
+
+            if shuffle: random.shuffle(item['answers'])
+            a = []
+            for letter, answer in zip('A B C D'.split(), item['answers']):
+                a.append('{}) {}'.format(letter, answer['text']))
+                if answer['correct'] == 'yes':
+                    solution = ET.Element('item')
+                    solution.text = str(ord(letter) - ord('A'))
+                    solutions.append(solution)
+
             sep = ';' + os.linesep
             answer = ET.Element('item')
-            answer.text = sep.join(map(lambda a: '{}) {}'.format(a['letter'], a['text']), item['answers']))
+            answer.text = sep.join(a)
             answers.append(answer)
 
             question = ET.Element('item')
@@ -40,7 +54,8 @@ def convert(file, prefix):
 
         return (
             parseString(ET.tostring(xmlq, encoding='utf-8', method='xml').decode()).toprettyxml(encoding='utf8', indent='    '),
-            parseString(ET.tostring(xmla, encoding='utf-8', method='xml').decode()).toprettyxml(encoding='utf8', indent='    ')
+            parseString(ET.tostring(xmla, encoding='utf-8', method='xml').decode()).toprettyxml(encoding='utf8', indent='    '),
+            parseString(ET.tostring(xmls, encoding='utf-8', method='xml').decode()).toprettyxml(encoding='utf8', indent='    ')
         )
 
 
@@ -52,24 +67,27 @@ if __name__ == '__main__':
     #   -> ../src/main/res/value/azf-answers.xml
     # $ python catalog-json-to-xml.py BZF.pdf ../src/main/res/value/
 
-    # you want to shuffle-answers-and-solutions.py afterwards
 
     parser = ArgumentParser()
     parser.add_argument('file')
     parser.add_argument('output')
     parser.add_argument('--prefix')
+    parser.add_argument('--shuffle', action='store_true')
 
     args = parser.parse_args()
     if args.prefix is None:
         args.prefix = p.splitext(p.basename(args.file))[0].lower()
 
-    questions, answers = convert(args.file, args.prefix)
+    questions, answers, solutions = convert(args.file, args.prefix, args.shuffle)
     path = p.dirname(args.file)
     with open(p.join(args.output, '{}_questions.xml'.format(args.prefix)), 'wb') as file:
         file.write(questions)
 
     with open(p.join(args.output, '{}_answers.xml'.format(args.prefix)), 'wb') as file:
         file.write(answers)
+
+    with open(p.join(args.output, '{}_solutions.xml'.format(args.prefix)), 'wb') as file:
+        file.write(solutions)
 
     print('All files writen to {}'.format(args.output))
 
