61164b99ffbe2be823fbbe675535592cc54a2b71
Gollth <thoregoll@googlemail.com>
2019-05-12 20:34:59 +0200

    add open speech settings preferences

--- app/src/main/java/de/tgoll/projects/bzf/SettingsActivity.java
+++ app/src/main/java/de/tgoll/projects/bzf/SettingsActivity.java
@@ -1,5 +1,6 @@
 package de.tgoll.projects.bzf;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
@@ -23,6 +24,24 @@ public class SettingsActivity extends PreferenceActivity {
             public boolean onPreferenceClick(Preference preference) {
                 settings.edit().remove("history").remove("SavedState").apply();
                 Toast.makeText(getApplicationContext(), getString(R.string.settings_reset_toast), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        Preference openSpeechSettingsButton = findPreference(getString(R.string.settings_open_speech_api));
+        openSpeechSettingsButton.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                startActivity(new Intent(android.provider.Settings.ACTION_VOICE_INPUT_SETTINGS));
+                return false;
+            }
+        });
+
+        Preference openTTSSettingsButton = findPreference(getString(R.string.settings_open_tts_api));
+        openTTSSettingsButton.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                startActivity(new Intent().setAction("com.android.settings.TTS_SETTINGS").setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
                 return false;
             }
         });
