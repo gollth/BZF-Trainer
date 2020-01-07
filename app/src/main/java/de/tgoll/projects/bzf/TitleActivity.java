07e3cd47906536573394ad215c49017b2afae813
Thore Goll <thoregoll@googlemail.com>
2020-01-07 13:29:19 +0100

    FIX: Correctly select Navigation Tab
    If new fragments are loaded from other fragments, currently the
    `BottomNavigationView` does not update correctly. Somehow there is a bug inside
    `setSelectedItemId()`. With copying the code from that method and remove on if
    check, this works

--- app/src/main/java/de/tgoll/projects/bzf/TitleActivity.java
+++ app/src/main/java/de/tgoll/projects/bzf/TitleActivity.java
@@ -64,6 +64,10 @@ public class TitleActivity extends AppCompatActivity {
         if (id.equals(getString(R.string.statistics))) return R.id.nav_stats;
         throw new InvalidParameterException("Fragment ID " + id + " unknown");
     }
+    private void setNavigation(int id) {
+        MenuItem item = navigation.getMenu().findItem(id);
+        if (item != null) item.setChecked(true);
+    }
     boolean showFragment(String id) {
         return showFragment(id, false);
     }
@@ -74,7 +78,7 @@ public class TitleActivity extends AppCompatActivity {
         settings.edit().putString("navigation", id).apply();
 
         int tab = getNavigationID(id);
-        if (forceLoad) navigation.setSelectedItemId(tab);
+        if (forceLoad) setNavigation(tab);
         switch(tab) {
             case R.id.nav_azf:   return load(new CatalogueFragment(this, "azf"));
             case R.id.nav_bzf:   return load(new CatalogueFragment(this, "bzf"));
